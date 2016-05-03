5ea33d45be86390d9636d5dc18a1d5efb779efcd
Simon Krogmann <simon.krogmann@student.hpi.de>
2016-05-03 16:21:09 +0200

    Add base project

---
+++ source/main.cpp
@@ -0,0 +1,9 @@
+#include <utilgpu/qt/Config.h>
+
+int main(int argc, char* argv[])
+{
+    std::string applicationName = "build-resume";
+    util::Config config{"simonkrogmann", applicationName};
+
+    return 0;
+}
